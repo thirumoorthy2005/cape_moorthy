da067fc14ef7df3e356c130a5b3f254c0e108325
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2021-02-05 20:03:35 +0530

    Code Cleanup

---
+++ src/main/java/com/capeelectric/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.capeelectric.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capeelectric.model.User;
+import com.capeelectric.service.impl.CustomUserDetailsService;
+import com.capeelectric.service.impl.UserDetailsServiceImpl;
+
+/**
+ * 
+ * @author capeelectricsoftware
+ *
+ */
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	@Autowired
+	private UserDetailsServiceImpl userService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@PostMapping("/addUser")
+	public String addUser(@RequestBody User user) {
+		String password = user.getPassword();
+		user.setPassword(passwordEncoder.encode(password));
+		userService.save(user);
+		return "user added successfully";
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@GetMapping("/fetchUser/{userName}")
+	public UserDetails fetchUser(@PathVariable("userName") String userName) {
+		return userDetailsService.loadUserByUsername(userName);
+	}
+}
